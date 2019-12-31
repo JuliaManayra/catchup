3b93154ad312b6f55ba9408ff9ad412504b0c4d3
JuliaManayra <35712868+JuliaManayra@users.noreply.github.com>
2019-12-31 17:23:59 -0300

    Add files via upload

---
+++ implementacao/catchupapi/src/main/java/com/julia/api/catchup/CatchupapiApplication.java
@@ -0,0 +1,13 @@
+package com.julia.api.catchup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatchupapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatchupapiApplication.class, args);
+	}
+
+}
---
+++ implementacao/catchupapi/src/test/java/com/julia/api/catchup/CatchupapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julia.api.catchup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatchupapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
