3d39b5378dcd89757c60bf1455616004e49a21c0
julia <julia@julia-300E5M-300E5L>
2019-12-31 22:03:29 -0300

    classe perfil

---
+++ implementacao/catchupapi/src/main/java/com/julia/api/catchup/dominio/Perfil.java
@@ -0,0 +1,57 @@
+package com.julia.api.catchup.dominio;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Perfil {
+
+	@Id
+	private Integer id;
+	
+	private String descricao;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Perfil other = (Perfil) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+	
+}
