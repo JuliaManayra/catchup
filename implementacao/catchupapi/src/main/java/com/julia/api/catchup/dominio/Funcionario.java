2dbb5756eaf92f5cf3c9e27f0d68f3c2a2a41440
julia <julia@julia-300E5M-300E5L>
2019-12-31 21:45:46 -0300

    classe funcionario

--- implementacao/catchupapi/src/main/java/com/julia/api/catchup/dominio/Funcionario.java
+++ implementacao/catchupapi/src/main/java/com/julia/api/catchup/dominio/Funcionario.java
@@ -1,5 +1,122 @@
 package com.julia.api.catchup.dominio;
 
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import org.hibernate.validator.constraints.br.CPF;
+
+@Entity
 public class Funcionario {
 
+	@Id
+	private Integer id;
+	private String nome;
+	@CPF
+	@Column(unique = true)
+	private String cpf;
+	
+	private String emailRecuperacao;
+	
+	private String senha;
+	private String senhaConfirmacao;
+	
+	private String filial;
+	
+	private Date nascimento;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getEmailRecuperacao() {
+		return emailRecuperacao;
+	}
+
+	public void setEmailRecuperacao(String emailRecuperacao) {
+		this.emailRecuperacao = emailRecuperacao;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getSenhaConfirmacao() {
+		return senhaConfirmacao;
+	}
+
+	public void setSenhaConfirmacao(String senhaConfirmacao) {
+		this.senhaConfirmacao = senhaConfirmacao;
+	}
+
+	public String getFilial() {
+		return filial;
+	}
+
+	public void setFilial(String filial) {
+		this.filial = filial;
+	}
+
+	public Date getNascimento() {
+		return nascimento;
+	}
+
+	public void setNascimento(Date nascimento) {
+		this.nascimento = nascimento;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Funcionario other = (Funcionario) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+	
+	
 }
